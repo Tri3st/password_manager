de9fd9edfccbd7f5e7cac5487689c49f16bf4c90
Martin van Diest <martin.van.diest@gmail.com>
2021-10-22 16:33:32 +0200

    implemented the TODO's still have to fix issue what to do if data ISNT correct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from password_generator import PasswordGenerator
 from password import Password
+import re
 
 my_data = []
 
@@ -14,14 +15,53 @@ def generate_random_password():
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def validate_website(website_string):
+    site_pattern = r"^(https?://)?(\w+.)?\w+.(aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|"+\
+              "aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|cz|de|dj|dk|dm|"+\
+              "do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|"+\
+              "je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mn|mn|mo|mp|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|n"+\
+              "f|ng|ni|nl|no|np|nr|nu|nz|nom|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ra|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz"+\
+              "|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|arpa)$"
+    site = re.compile(site_pattern)
+    if not (6 < len(website_string) < 50):
+        return False
+    return site.match(website_string) != None
+
+
+def validate_email(email_string):
+    if not (6 < len(email_string) < 50):
+        return False
+    mail_pattern = r"^(\w+.?\w*.?\w*)@\w+.(aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|"+\
+              "aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|cz|de|dj|dk|dm|"+\
+              "do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|"+\
+              "je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mn|mn|mo|mp|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|n"+\
+              "f|ng|ni|nl|no|np|nr|nu|nz|nom|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ra|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz"+\
+              "|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|arpa)$"
+    mail = re.compile(mail_pattern)
+    return mail.match(email_string)
+
+
+def validate_password(password_string):
+    if not (6 < len(password_string) < 20):
+        return False
+    return True
+
+
 def add_info():
-    # TODO validate info in Entry boxes and if all complete make and save password details
-    pass
-
-
-def save_password_details():
-    # TODO get validated info and make an object to store data in
-    my_pass = Password()
+    unvalidated_website = input_website.get()
+    if validate_website(unvalidated_website):
+        validated_website = unvalidated_website
+    unvalidated_email = input_username.get()
+    if validate_email(unvalidated_email):
+        validated_email = unvalidated_email
+    unvalidated_password = input_password.get()
+    if validate_password(unvalidated_password):
+        validated_password = unvalidated_password
+    my_validated_info = Password()
+    my_validated_info.set_all(validated_website, validated_email, validated_password)
+    my_data.append(my_validated_info)
+    print(my_data)
+    print(my_validated_info)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
